6e838992f054c9d8267c38f4ffca4c5e89fb16e7
Maika Kanaka <bif@maika-kanaka.id>
2019-10-19 16:28:34 +0700

    view for page lottery and update db for the winner

--- pages/lottery.py
+++ pages/lottery.py
@@ -1,10 +1,11 @@
 import sys 
 import time
+import datetime 
 import random
 
 sys.path.append("../")
 
-from libraries import UI
+from libraries import UI, Config
 from models.member_model import MemberModel
 
 class LotteryPage:
@@ -13,11 +14,20 @@ class LotteryPage:
         self.memberModel = MemberModel()
 
     def confirm(self):
-        c = input("ARE YOU READY (Y/n) ?")
+        c = input("ARE YOU READY (Y/n)> ")
 
-        if c.lower() == 'y':
+        if c.lower() == 'n':
+            Config.route = 'home'
+            return False
+        else:
             # get data 
             query = self.memberModel.data()
+
+            # valid: data member is must exists
+            if len(query) < 1:
+                print("You do not have any members")
+                self.confirm()
+                return False
 
             # fill to list
             members = []
@@ -40,11 +50,16 @@ class LotteryPage:
             winner = random.randrange(0, len(members))
             the_winner = self.memberModel.detail(pk = members[winner])
 
-        UI.figlet(text="THE WINNER IS ......", font = "cybersmall")
+        # UI.figlet(text="THE WINNER IS ......", font = "cybersmall")
+        print("The winner is: ")
+        print("\n", end="")
         UI.figlet(text = str(the_winner[3]), font = "twopoint")
         
         # ask 
-        c = input("Save the winner or run again?")
-        
+        c = input("Save the winner (Y/n)> ")
+        c = c.lower()
+
         # update the winner 
+        now = datetime.datetime.now()
+        self.memberModel.update_the_winner(the_winner[2], now)
         
